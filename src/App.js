f993350ab36802cdf94741c39817f47f708c1a8e
MW <41174111+calientacabezas@users.noreply.github.com>
2019-07-10 09:19:39 +0900

    .

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import main from "./main";
+
+class App extends Component {
+  render() {
+    return (
+      <main />
+      
+      )
+  }
+}
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main.scss';
+
+const main = () => {
+  return (
+    <div>
+      <h4 class="searchText">search</h4>
+      <input type="text" name="searchBox" value="enter the movie in Hive"></input>
+    </div>
+  );
+};
+
+export default main;
